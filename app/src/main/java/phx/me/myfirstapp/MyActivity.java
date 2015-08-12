21b775e8b2bdbc57360deecbd867a72b978badd2
Alwayswithme <phx13ye@gmail.com>
2015-08-12 18:38:44 +0800

    start another activity

--- app/src/main/java/phx/me/myfirstapp/MyActivity.java
+++ app/src/main/java/phx/me/myfirstapp/MyActivity.java
@@ -1,11 +1,16 @@
 package phx.me.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "phx.me.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,14 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
